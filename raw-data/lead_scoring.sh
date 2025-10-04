34e4e800263e73270566c3458af7e82b2ba8f36d
Rafael Kuttner <r.i.kuttner@gmail.com>
2025-10-03 18:38:49 -0700

    solutions to draft homework week 3 logistic regression

---
+++ raw-data/lead_scoring.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Lead scoring Bank Marketing data for week 3 homework
+# Source is a Kaggle competition https://www.kaggle.com/ashydv/leads-dataset
+# -- rename to 'lead_scoring' for clarity
+wget -O data/lead_scoring.csv https://raw.githubusercontent.com/alexeygrigorev/datasets/master/course_lead_scoring.csv
